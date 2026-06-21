4ff48bd6f54f60cd6f5fc4d081a532147a449dc0
ratewatchng-design <ratewatch.ng@gmail.com>
2026-06-21 09:40:23 +0100

    Delete ui directory

--- ui/referral_menu.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-
-def referral_keyboard() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton("🏠 Main Menu", callback_data="home"),
-        ],
-    ])
